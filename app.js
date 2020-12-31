a6e3834e5ad853d72aeafcd6b0603e587798facd
Robert Burek <robertburek@wp.pl>
2020-12-31 13:52:02 +0100

    Size ball

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ctx = cnv.getContext('2d');
 const cnvW = cnv.width = 1200;
 const cnvH = cnv.height = 600;
 
+const ballSize = 16;
+
 function ball(){
     ctx.arc(cnvW/2 - ballSize/2,cnvH/2 - ballSize/2, ballSize/2, 0, 2*Math.PI);
     ctx.fillStyle = 'lime';
