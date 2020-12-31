668fe0f4cb2be2c28ac790e0852a0b89ce17c1c7
Robert Burek <robertburek@wp.pl>
2020-12-31 13:36:39 +0100

    Ball i court

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ const ctx = cnv.getContext('2d');
 const cnvW = cnv.width = 1200;
 const cnvH = cnv.height = 600;
 
+function ball(){
+    ctx.arc(cnvW/2 - ballSize/2,cnvH/2 - ballSize/2, ballSize/2, 0, 2*Math.PI);
+    ctx.fillStyle = 'lime';
+    ctx.fill();
+}
+
+function court() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,cnvW,cnvH);
+}
+
+court();
+ball();
